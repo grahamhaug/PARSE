fc318d8a9ce0b2dd216d048d8c374ac03543b921
Graham Haug <49004818+grahamhaug@users.noreply.github.com>
2022-03-25 14:10:50 -0500

    Update SDF_stitcher.py

--- tools/SDF_stitcher.py
+++ tools/SDF_stitcher.py
@@ -7,11 +7,11 @@ Outputs a new sdf containing all of the located sdfs
 sdf_files = [file for file in os.listdir('.') if os.path.isfile(file) and file.endswith('.sdf')]
 
 if sdf_files:
-	print(f"\nFound {len(sdf_files)} .sdf files in local directory:")
+	print(f"\nFound {len(sdf_files)} .sdf in local directory:")
 	for sdf_file in sdf_files:
 		print(sdf_file)
 else:
-	print("No .sdf files found in local directory.")
+	print("No .sdfs found in local directory.")
 
 #ask user for an output name - will create example.png in local dir. 
 outputName = input("\nEnter desired filename for output sdf (no extension): ")
